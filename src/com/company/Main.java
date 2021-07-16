9c39f8f6ee2f6b8df880986a51442c1bb1e48f30
Muhammad Talha <muhammad.talha@confiz.local>
2021-07-16 19:21:24 +0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,99 @@
+package com.company;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Main implements Runnable{
+
+    public void run() {}
+
+    private static final HashSet<String> allCities = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        String path = "src/com/company/Candidates.csv";
+        List<Candidate> candidates = readCandidatesFromCSV(path);
+
+        candidates.sort(Comparator.comparing(Candidate::getCity));
+
+        separateCities(candidates);
+    }
+
+    private static List<Candidate> readCandidatesFromCSV(String filePath) {
+        List<Candidate> candidates= new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+            br.readLine();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] data = line.split(",");
+                allCities.add(data[4]);
+                Candidate c = createCandidate(data);
+                candidates.add(c);
+
+                line = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! File not found.");
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (ParseException pe) {
+            pe.getCause();
+        }
+
+        return candidates;
+    }
+
+    private static Candidate createCandidate(String[] dataFromCSVLine) throws ParseException {
+        // CSV Format: ID, Name, Gender, Age, City, DOB
+        int id = Integer.parseInt(dataFromCSVLine[0]);
+        String name = dataFromCSVLine[1];
+        char gender = dataFromCSVLine[2].charAt(0);
+        int age = Integer.parseInt(dataFromCSVLine[3]);
+        String city = dataFromCSVLine[4];
+        String dateString = dataFromCSVLine[5];
+        Date date = new SimpleDateFormat("dd/MM/yy").parse(dateString);
+
+
+        return new Candidate(id, name, gender, age, city, date);
+    }
+
+
+    private static void separateCities(List<Candidate> candidates)
+    {
+        for(String cityName : allCities){
+            System.out.println(cityName);
+            writeToCSV(candidates, cityName);
+        }
+    }
+
+    private static void writeToCSV(List<Candidate> candidates, String cityName) {
+
+        List<Candidate> written = new ArrayList<>();
+
+        try (FileWriter csvWriter = new FileWriter(cityName + "-data.csv")) {
+            csvWriter.append("id, Name, Gender, Age, City, DOB");
+            csvWriter.append("\n");
+
+            for (Candidate c : candidates) {
+                if (c.getCity().equals(cityName)) {
+                    csvWriter.append(c.toString());
+                    csvWriter.append("\n");
+                    written.add(c);
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        candidates.removeAll(written);
+    }
+}
+
+
